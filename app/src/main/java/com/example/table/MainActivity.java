e75dbd85fa09e8aebaef37795282ae4bc6618a87
Mezeo-22 <ktoto.tam2005@mail.ru>
2023-02-28 10:46:22 +0900

    обработка нажатия

--- app/src/main/java/com/example/table/MainActivity.java
+++ app/src/main/java/com/example/table/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.table;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView nav_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        nav_view = findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.id_my_ads:
+                Toast.makeText(this, "Pressed id my ads", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_cars_ads:
+                Toast.makeText(this, "Pressed id cars ads", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_pc_ads:
+                Toast.makeText(this, "Pressed id pc ads", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_smartphone_ads:
+                Toast.makeText(this, "Pressed id smartphone ads", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_dm_ads:
+                Toast.makeText(this, "Pressed id dm ads", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_sign_up:
+                Toast.makeText(this, "Pressed id sign up", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_sign_in:
+                Toast.makeText(this, "Pressed id sign in", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_sign_out:
+                Toast.makeText(this, "Pressed id sign out", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
