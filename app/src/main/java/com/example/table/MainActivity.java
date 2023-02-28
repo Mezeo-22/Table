43c0d22c5f84f8333d43296eab30f899b6221cc2
Mezeo-22 <ktoto.tam2005@mail.ru>
2023-02-28 11:35:27 +0900

    экран регистрации

--- app/src/main/java/com/example/table/MainActivity.java
+++ app/src/main/java/com/example/table/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.table;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -45,9 +50,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Toast.makeText(this, "Pressed id dm ads", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_up:
+                signupdialog(R.string.sign_up, R.string.sign_up_button);
                 Toast.makeText(this, "Pressed id sign up", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_in:
+                signupdialog(R.string.sign_in, R.string.sign_in_button);
                 Toast.makeText(this, "Pressed id sign in", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_out:
@@ -55,5 +62,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         return true;
+    }
+
+    private void signupdialog(int title, int buttontitle) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.sign_up_layout, null);
+        dialogBuilder.setView(dialogView);
+        TextView titleTextView = dialogView.findViewById(R.id.tvAlertTitle);
+        titleTextView.setText(title);
+        Button b = dialogView.findViewById(R.id.buttonSignUp);
+        b.setText(buttontitle);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
