b88b214403d0b29f0ca4e24218373bac0237886a
Mezeo-22 <ktoto.tam2005@mail.ru>
2023-03-03 11:17:40 +0900

    регистрация

--- app/src/main/java/com/example/table/MainActivity.java
+++ app/src/main/java/com/example/table/MainActivity.java
@@ -3,20 +3,33 @@ package com.example.table;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private NavigationView nav_view;
+    private DrawerLayout drawerLayout;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +40,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void init() {
         nav_view = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawerLayout);
         nav_view.setNavigationItemSelectedListener(this);
+        drawerLayout.openDrawer(GravityCompat.START);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 
     @Override
@@ -50,11 +73,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Toast.makeText(this, "Pressed id dm ads", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_up:
-                signupdialog(R.string.sign_up, R.string.sign_up_button);
+                signupdialog(R.string.sign_up, R.string.sign_up_button, 0);
                 Toast.makeText(this, "Pressed id sign up", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_in:
-                signupdialog(R.string.sign_in, R.string.sign_in_button);
+                signupdialog(R.string.sign_in, R.string.sign_in_button, 1);
                 Toast.makeText(this, "Pressed id sign in", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_out:
@@ -64,22 +87,46 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-    private void signupdialog(int title, int buttontitle) {
+    private void signupdialog(int title, int buttontitle, int index) {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         LayoutInflater inflater = this.getLayoutInflater();
         View dialogView = inflater.inflate(R.layout.sign_up_layout, null);
         dialogBuilder.setView(dialogView);
         TextView titleTextView = dialogView.findViewById(R.id.tvAlertTitle);
+        EditText edEmail = dialogView.findViewById(R.id.edEmail);
+        EditText edPassword = dialogView.findViewById(R.id.edPassword);
         titleTextView.setText(title);
         Button b = dialogView.findViewById(R.id.buttonSignUp);
         b.setText(buttontitle);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (index==0) {
+                    signUp(edEmail.getText().toString(), edPassword.getText().toString());
+                }
             }
         });
         AlertDialog dialog = dialogBuilder.create();
         dialog.show();
+    }
+
+    private void signUp (String email, String password) {
+        if (!email.equals("") && !password.equals("")) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("MyLogMain", "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                            } else {
+                                Log.w("MyLogMain", "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Authentification failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "Email или Password пустой", Toast.LENGTH_SHORT).show();
+        }
     }
 }
